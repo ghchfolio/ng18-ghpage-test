6442cfb9fc4529765df5d12957ed42cd798ea62e
ghchfolio <ghchfolio@gmail.com>
2024-06-24 11:37:58 -0500

    docs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,16 +2,31 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  template: `
+    selector: 'app-root',
+    standalone: true,
+    imports: [RouterOutlet],
+    template: `
     <h3>Angular &amp; 18 ghPages</h3>
     <p>Quick test.</p>
+    <ol>
+        <li>repo must be public 👎</li>
+        <li>npm i angular-cli-ghpages --save-dev</li>
+        <li>ng build --base-href "/REPOSITORY_NAME/"</li>
+        <li>npx angular-cli-ghpages --dir=dist/[PROJECTNAME]/browser</li>
+    </ol>
+
+    <h4>Links</h4>
+    <ul>
+        <li>
+            <a href="https://github.com/angular-schule/angular-cli-ghpages/blob/main/docs/README_standalone.md" target="_blank">
+                angular-cli-ghpages
+            </a>
+        </li>
+    </ul>
     <router-outlet />
   `,
-  styles: [],
+    styles: [],
 })
 export class AppComponent {
-  title = 'ng18-ghpage-test';
+    title = 'ng18-ghpage-test';
 }
